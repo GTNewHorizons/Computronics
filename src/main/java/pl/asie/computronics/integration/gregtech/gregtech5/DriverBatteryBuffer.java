f0f741542fc5c647dbc6275ef9ae21c4452e2336
NotAPenguin <michiel.vandeginste@gmail.com>
2024-09-03 15:46:37 +0200

    update gt, rename

--- src/main/java/pl/asie/computronics/integration/gregtech/gregtech5/DriverBatteryBuffer.java
+++ src/main/java/pl/asie/computronics/integration/gregtech/gregtech5/DriverBatteryBuffer.java
@@ -6,8 +6,8 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import gregtech.api.metatileentity.BaseMetaTileEntity;
-import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_BasicBatteryBuffer;
-import gregtech.api.util.GT_ModHandler;
+import gregtech.api.metatileentity.implementations.MTEBasicBatteryBuffer;
+import gregtech.api.util.GTModHandler;
 import ic2.api.item.ElectricItem;
 import ic2.api.item.IElectricItem;
 import li.cil.oc.api.machine.Arguments;
@@ -37,15 +37,14 @@ public class DriverBatteryBuffer extends DriverSidedTileEntity {
                 doc = "function(slot:number):number; Returns the amount of stored EU in the battery in the specified slot")
         public Object[] getBatteryCharge(Context c, Arguments a) {
             int slot = a.checkInteger(0);
-            if (slot <= 0
-                    || slot > ((GT_MetaTileEntity_BasicBatteryBuffer) tile.getMetaTileEntity()).mInventory.length) {
+            if (slot <= 0 || slot > ((MTEBasicBatteryBuffer) tile.getMetaTileEntity()).mInventory.length) {
                 return new Object[] { null, "slot does not exist" };
             }
-            if (((GT_MetaTileEntity_BasicBatteryBuffer) tile.getMetaTileEntity()).mInventory[slot - 1] == null) {
+            if (((MTEBasicBatteryBuffer) tile.getMetaTileEntity()).mInventory[slot - 1] == null) {
                 return new Object[] { null, "slot is empty" };
             }
-            ItemStack stack = ((GT_MetaTileEntity_BasicBatteryBuffer) tile.getMetaTileEntity()).mInventory[slot - 1];
-            if (GT_ModHandler.isElectricItem(stack)) {
+            ItemStack stack = ((MTEBasicBatteryBuffer) tile.getMetaTileEntity()).mInventory[slot - 1];
+            if (GTModHandler.isElectricItem(stack)) {
                 return new Object[] { ElectricItem.manager.getCharge(stack) };
             } else {
                 return new Object[] { null, "item in slot is not electric" };
@@ -56,17 +55,16 @@ public class DriverBatteryBuffer extends DriverSidedTileEntity {
                 doc = "function(slot:number):number; Returns the max amount of stored EU in the battery in the specified slot")
         public Object[] getMaxBatteryCharge(Context c, Arguments a) {
             int slot = a.checkInteger(0);
-            if (slot <= 0
-                    || slot > ((GT_MetaTileEntity_BasicBatteryBuffer) tile.getMetaTileEntity()).mInventory.length) {
+            if (slot <= 0 || slot > ((MTEBasicBatteryBuffer) tile.getMetaTileEntity()).mInventory.length) {
                 return new Object[] { null, "slot does not exist" };
             }
             // System.out.println(((GT_MetaTileEntity_BasicBatteryBuffer) tile.getMetaTileEntity()).mInventory[slot - 1]
             // == null);
-            if (((GT_MetaTileEntity_BasicBatteryBuffer) tile.getMetaTileEntity()).mInventory[slot - 1] == null) {
+            if (((MTEBasicBatteryBuffer) tile.getMetaTileEntity()).mInventory[slot - 1] == null) {
                 return new Object[] { null, "slot is empty" };
             }
-            ItemStack stack = ((GT_MetaTileEntity_BasicBatteryBuffer) tile.getMetaTileEntity()).mInventory[slot - 1];
-            if (GT_ModHandler.isElectricItem(stack)) {
+            ItemStack stack = ((MTEBasicBatteryBuffer) tile.getMetaTileEntity()).mInventory[slot - 1];
+            if (GTModHandler.isElectricItem(stack)) {
                 return new Object[] { ((IElectricItem) stack.getItem()).getMaxCharge(stack) };
             } else {
                 return new Object[] { null, "item in slot is not electric" };
@@ -84,8 +82,7 @@ public class DriverBatteryBuffer extends DriverSidedTileEntity {
     public boolean worksWith(World world, int x, int y, int z, ForgeDirection side) {
         TileEntity tileEntity = world.getTileEntity(x, y, z);
         return (tileEntity != null) && tileEntity instanceof BaseMetaTileEntity
-                && ((BaseMetaTileEntity) tileEntity)
-                        .getMetaTileEntity() instanceof GT_MetaTileEntity_BasicBatteryBuffer;
+                && ((BaseMetaTileEntity) tileEntity).getMetaTileEntity() instanceof MTEBasicBatteryBuffer;
     }
 
     @Override
