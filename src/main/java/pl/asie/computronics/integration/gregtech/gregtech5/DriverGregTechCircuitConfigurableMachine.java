6e87a092ea7eccf1a81c7aaecedca53e90c5dfa7
Martin Robertz <dream-master@gmx.net>
2025-05-18 18:35:48 +0200

    sa

--- src/main/java/pl/asie/computronics/integration/gregtech/gregtech5/DriverGregTechCircuitConfigurableMachine.java
+++ src/main/java/pl/asie/computronics/integration/gregtech/gregtech5/DriverGregTechCircuitConfigurableMachine.java
@@ -1,16 +1,15 @@
 package pl.asie.computronics.integration.gregtech.gregtech5;
 
-import gregtech.api.interfaces.IConfigurationCircuitSupport;
-import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
+import gregtech.api.interfaces.IConfigurationCircuitSupport;
+import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.metatileentity.BaseMetaTileEntity;
 import gregtech.api.util.GTUtility;
 import gregtech.common.items.ItemIntegratedCircuit;
-
 import li.cil.oc.api.machine.Arguments;
 import li.cil.oc.api.machine.Callback;
 import li.cil.oc.api.machine.Context;
@@ -23,6 +22,7 @@ public class DriverGregTechCircuitConfigurableMachine extends DriverSidedTileEnt
     private static final int NO_CONFIG = -1;
 
     public static class ManagedEnvironmentGregTechCircuitConfig extends ManagedEnvironmentOCTile<IMetaTileEntity> {
+
         public static final int MAX_CIRCUIT_NUMBER = 24;
 
         public ManagedEnvironmentGregTechCircuitConfig(IMetaTileEntity tile, String name) {
@@ -41,44 +41,52 @@ public class DriverGregTechCircuitConfigurableMachine extends DriverSidedTileEnt
             return -1;
         }
 
-        @Callback(doc = "function():number; Gets the circuit configuration. Returns -1 if no circuit or not applicable.")
+        @Callback(
+                doc = "function():number; Gets the circuit configuration. Returns -1 if no circuit or not applicable.")
         public Object[] getCircuitConfiguration(Context context, Arguments args) {
             if (!(this.tile instanceof IConfigurationCircuitSupport)) {
-                return new Object[]{null, "Machine does not support circuit configuration."};
+                return new Object[] { null, "Machine does not support circuit configuration." };
             }
             int circuitSlot = getCircuitSlot();
             if (circuitSlot < 0 || circuitSlot >= this.tile.getSizeInventory()) {
-                return new Object[]{null, "Invalid circuit slot index."};
+                return new Object[] { null, "Invalid circuit slot index." };
             }
 
             ItemStack stack = this.tile.getStackInSlot(circuitSlot);
             if (stack != null && stack.getItem() instanceof ItemIntegratedCircuit) {
-                return new Object[]{stack.getItemDamage()};
+                return new Object[] { stack.getItemDamage() };
             }
-            return new Object[]{NO_CONFIG};
+            return new Object[] { NO_CONFIG };
         }
 
-        @Callback(doc = "function(config:number):boolean or (nil, string); Sets the circuit configuration. Use -1 to remove circuit. Returns true on success.")
+        @Callback(
+                doc = "function(config:number):boolean or (nil, string); Sets the circuit configuration. Use -1 to remove circuit. Returns true on success.")
         public Object[] setCircuitConfiguration(Context context, Arguments args) {
             if (!(this.tile instanceof IConfigurationCircuitSupport)) {
-                return new Object[]{null, "Machine does not support circuit configuration."};
+                return new Object[] { null, "Machine does not support circuit configuration." };
             }
             int circuitSlot = getCircuitSlot();
             if (circuitSlot < 0 || circuitSlot >= this.tile.getSizeInventory()) {
-                return new Object[]{null, "Invalid circuit slot index."};
+                return new Object[] { null, "Invalid circuit slot index." };
             }
 
             int config = args.checkInteger(0);
 
             if (config == NO_CONFIG) {
                 this.tile.setInventorySlotContents(circuitSlot, null);
-                return new Object[]{true};
+                return new Object[] { true };
             } else if (config >= 1 && config <= MAX_CIRCUIT_NUMBER) {
                 ItemStack circuitStack = GTUtility.getIntegratedCircuit(config);
                 this.tile.setInventorySlotContents(circuitSlot, circuitStack);
-                return new Object[]{true};
+                return new Object[] { true };
             } else {
-                return new Object[]{null, "Invalid circuit configuration value: " + config + ". Must be between 1 and " + MAX_CIRCUIT_NUMBER + ", or " + NO_CONFIG + "."};
+                return new Object[] { null,
+                        "Invalid circuit configuration value: " + config
+                                + ". Must be between 1 and "
+                                + MAX_CIRCUIT_NUMBER
+                                + ", or "
+                                + NO_CONFIG
+                                + "." };
             }
         }
     }
